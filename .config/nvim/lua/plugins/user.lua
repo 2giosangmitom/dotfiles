5a4db1a255c19584d17aa8ca083399d121ec5a33
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-03 10:03:27 +0700

    update nvim config

---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "2giosangmitom/nightfall.nvim",
+    dir = "~/Workspace/nightfall.nvim/",
+    opts = {},
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+
+  {
+    "catppuccin",
+    enabled = false,
+  },
+
+  {
+    "stevearc/overseer.nvim",
+    opts = {
+      templates = { "builtin", "template.build_cpp", "template.run_bin" },
+    },
+  },
+}
