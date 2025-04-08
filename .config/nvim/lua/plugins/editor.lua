c56606ac2afb7c38d99dd482c470a48f608e8d37
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-08 10:58:46 +0700

    update config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,4 +7,45 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.trailspace",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.visits",
+    keys = {
+      { "<leader>vp", "<cmd>lua MiniVisits.select_path()<cr>", desc = "Select path" },
+      { "<leader>vl", "<cmd>lua MiniVisits.select_label()<cr>", desc = "Select label" },
+      { "<leader>va", "<cmd>lua MiniVisits.add_label()<cr>", desc = "Add label" },
+      { "<leader>vd", "<cmd>lua MiniVisits.remove_label()<cr>", desc = "Delete label" },
+    },
+    opts = {},
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        {
+          mode = { "n" },
+          { "<leader>v", group = "visits" },
+        },
+      },
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          Snacks.picker.projects({
+            dev = { "~/Workspace" },
+            recent = false,
+          })
+        end,
+        desc = "Projects",
+      },
+    },
+  },
 }
