ea3d95094d7fe9e250841d68fe9fdbf7e87e1742
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-13 11:55:42 +0700

    nvim: setup mini.icons

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,11 +7,16 @@ return {
 	{
 		"echasnovski/mini.tabline",
 		event = { "BufNewFile", "BufReadPost" },
+		dependencies = { "echasnovski/mini.icons" },
 		opts = {},
 	},
 	{
 		"echasnovski/mini.statusline",
 		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"echasnovski/mini.icons",
 		opts = {},
 	},
 }
