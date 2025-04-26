3713ebff03146f6721faef25e6c9b1f654a9bd64
Vo Quang Chien <voquangchien.dev@proton.me>
2025-04-26 16:18:07 +0700

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,1 @@
-# Disable greeting message
-set fish_greeting
-
-# Enable starship integration
 starship init fish | source
-
-# Enable zoxide integration
-zoxide init fish | source
-
-# Enable fzf integration
-fzf --fish | source
-
-set PATH $PATH $HOME/.local/bin/
