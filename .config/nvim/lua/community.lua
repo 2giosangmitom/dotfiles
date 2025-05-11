2e376be413b159e5ef09d57cb54384d4acef2587
Vo Quang Chien <voquangchien.dev@proton.me>
2025-05-11 15:24:42 +0700

    nvim: update nvim config

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -2,7 +2,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.game.leetcode-nvim" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.motion.flash-nvim" },
 }
