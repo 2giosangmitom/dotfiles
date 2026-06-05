9dcc005c6121cfcb1a0c6b7a422dcf9665c263fc
Vo Quang Chien <2giosangmitom@proton.me>
2026-06-05 20:19:57 +0700

    update

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+set -U fish_greeting
+set -U fish_user_paths ~/.local/bin ~/.local/share/solana/install/active_release/bin
+
+zoxide init fish | source
+starship init fish | source
+fzf --fish | source
+mise activate fish | source
+
+alias ls="eza --color=auto --group-directories-first"
+alias la="ls -a"
+alias ll="ls -l"
+alias lla="ls -la"
+alias lt="ls --tree"
